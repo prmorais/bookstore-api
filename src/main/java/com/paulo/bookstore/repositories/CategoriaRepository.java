4fe47072a6bd8d4c30dcbf9eba77efb24bda4e53
Paulo Roberto <prmorais1302@gmail.com>
2021-01-24 18:21:29 -0300

    Implementando resository de Categoria e Livro

---
+++ src/main/java/com/paulo/bookstore/repositories/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.paulo.bookstore.repositories;
+
+import com.paulo.bookstore.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+}
---
+++ src/main/java/com/paulo/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.paulo.bookstore.repositories;
+
+import com.paulo.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
